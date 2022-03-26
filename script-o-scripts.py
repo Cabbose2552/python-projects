68d8af2c173097d3a34fe17d3ea25e861b1fc592
darthdestryer001 <60184741+darthdestryer001@users.noreply.github.com>
2022-03-25 22:45:04 -0400

    Add files via upload

--- script-o-scripts.py
+++ script-o-scripts.py
@@ -1,31 +1,31 @@
 import time
 import random
-import cv2
-import gens
-import mathops
-import testops
-
+import math
+import operator
 from tkinter import *
 
 pyver = '3.10.2'
 version = '1.0.0'
-pcnt = 1
 cnt = 1
-mult = 2
-multnum = 1
+mlt = 2
+mltnum = 1
 
 while True:
         script = input("what script would you like? type list to display a list of scripts:")
 
         if script == ("list"):
                 print("-----------------------------------------------------------------")
-                print("|link, plays a simple blinking animation                        |")
+                print("|blink, plays a simple blinking animation                       |")
                 print("|                                                               |")
                 print("|cnt, counts to infinity                                        |")
                 print("|                                                               |")
-                print("|pcnt, displays prime numbers to infinity(wip)                  |")
+                print("|info, display info about script                                |")
                 print("|                                                               |")
-                print("|mult, displays numbers multiplied by the entered value         |")
+                print("|run skyrim, does what it says                                  |")
+                print("|                                                               |")
+                print("|clear, clears screen(mostly)                                   |")
+                print("|                                                               |")
+                print("|mlt, displays numbers multiplied by the entered value          |")
                 print("|                                                               |")
                 print("|stress-test-1/2, provides a simple stress test for your system.|")
                 print("|                                                               |")
@@ -42,6 +42,16 @@ while True:
             while True:
                 cnt = cnt + 1
                 print(cnt)
+                
+        elif script == ("cnto"):
+            while True:
+                strt = int(input("input number to count from:"))
+                numi = int(input("input number to count to:"))
+
+                print(strt)
+                for x in range(numi-strt):
+                    print(strt+1)
+                    strt = strt+1
 
         elif script == ("xgen"):
                 print("")
@@ -95,14 +105,14 @@ while True:
                         print(g(4)+"-"+g(4)+"-"+g(4))
                 print("")
 
-        elif script == ("mult"):
+        elif script == ("mlt"):
             print("")
-            multnum = input("enternumber to mupltiply by:")
+            mltnum = input("enternumber to mupltiply by:")
             print("")
-            multnumint = int(multnum)
+            mltnumint = int(mltnum)
             while True:
-                mult= mult * multnumint
-                print(mult)
+                mlt= mlt * mltnumint
+                print(mlt)
 
         elif script == ("info"):
                 print("======================")
@@ -115,13 +125,13 @@ while True:
                 
         elif script == ("stress-test-1"):
             while True:
-                mult= mult * 99999999999
-                print(mult)
+                mlt= mlt * 99999999999
+                print(mlt)
                 
         elif script == ("stress-test-2"):
             while True:
-                mult= mult * mult
-                print(mult)
+                mlt= mlt * mlt
+                print(mlt)
 
         elif script == ("blink"):
                 for x in range(10):
@@ -134,12 +144,14 @@ while True:
                         print("owo")
                         time.sleep(0.1)
 
-        elif script == ("pcnt"):
-            print("this is a work in progress")
-            
         elif script == ("clear"):
-            for x in range(40):
+            for x in range(50):
                     print("")
+
+        elif script == ("run skyrim"):
+                print("*faint dying sounds*")
+                time.sleep(10)
+                int("working")
 
         elif script == ("win"):
             winw = input("window width:")
@@ -153,10 +165,49 @@ while True:
             # set window background color
             window.configure(bg='lightblue')
             window.mainloop()
+        elif script == ("mathops"):
+                ops = {'+':operator.add,
+                    '-':operator.sub,
+                    '*':operator.mul,
+                    '/':operator.truediv}
+                na = random.randint(1, 20)
+                nb = random.randint(1, 20)
+                op = random.choice(list(ops.keys()))
+                txt = ("what is", na, op, nb,"?")
+                ans = ops.get(op)(na,nb)
 
-        elif script == ("penis") or ("dick") or ("cock") or ("balls"):
-                print("tee hee, thats a bad word")
-
+                root = Tk()
+                root.geometry("300x320")
+                root.title("Q&A")
+                 
+                def Take_input():
+                    INPUT = inputtxt.get("1.0", "end-1c")
+                    print(INPUT)
+                    if(int(INPUT) == ans):
+                        Output.insert(END, 'Correct')
+                    else:
+                        Output.insert(END, "Wrong answer")
+                     
+                l = Label(text = txt)
+                inputtxt = Text(root, height = 10,
+                                width = 25,
+                                bg = "light yellow")
+                 
+                Output = Text(root, height = 5,
+                              width = 25,
+                              bg = "light cyan")
+                 
+                Display = Button(root, height = 2,
+                                 width = 20,
+                                 text ="Show",
+                                 command = lambda:Take_input())
+                 
+                l.pack()
+                inputtxt.pack()
+                Display.pack()
+                Output.pack()
+                 
+                mainloop()
         else:
             print("######################")
             print("#error invalid target#")
